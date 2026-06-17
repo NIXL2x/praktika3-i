f092646c3e0cf91744891a1a909d8b7f8f2c5d94
NIXL <vihrov.nik2005@gmail.com>
2026-06-17 14:25:13 +0500

    pop2

--- LICENSES/pr09/demo-project/app.py
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Nikita Vikhrov
-# SPDX-License-Identifier: MIT
-
-import requests
-print("demo app")
--- LICENSES/pr09/report.md
+++
@@ -1,5 +0,0 @@
-<!--
-SPDX-FileCopyrightText: 2026 Nikita Vikhrov vihrov.nik2005@gmail.com
-
-SPDX-License-Identifier: MIT
--->
